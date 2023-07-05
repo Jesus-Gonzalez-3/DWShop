304c3586604cec967622636f329b1d98ce8dc9a6
MXJGaona01 <jesus.gaona@na.denso.com>
2023-07-04 18:08:34 -0600

    Login View View Model

---
+++ src/Client/Mobile/DWShop.Client.Mobile/Models/LoginModel.cs
@@ -0,0 +1,26 @@
+﻿using DWShop.Client.Mobile.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWShop.Client.Mobile.Models
+{
+    public class LoginModel : ObservableObject
+    {
+        private string userName;
+        public string UserName
+        {
+            get => userName;
+            set => SetProperty(ref userName, value);
+        }
+
+        private string password;
+        public string Password
+        {
+            get => password;
+            set => SetProperty(ref password, value);
+        }
+    }
+}
